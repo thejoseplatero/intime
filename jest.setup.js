8b51e29eb836903d598a524a5e895be588398bd4
J <jj@Js-MacBook-Pro.local>
2025-10-26 14:29:44 -0400

    Remove Reanimated - simplify to basic React Native only

--- jest.setup.js
+++ jest.setup.js
@@ -1,24 +1,3 @@
-import '@testing-library/jest-native/extend-expect';
-
-// Mock React Native modules
-jest.mock('react-native-reanimated', () => {
-  const RNReanimated = {
-    default: {
-      spring: () => ({ start: jest.fn() }),
-      timing: () => ({ start: jest.fn() }),
-    },
-    useSharedValue: (val: any) => ({ value: val }),
-    useAnimatedStyle: (callback: any) => ({ value: callback() }),
-    withSpring: (val: any) => val,
-    withTiming: (val: any) => val,
-    interpolate: () => 0,
-    Extrapolation: { CLAMP: 'clamp' },
-  };
-  return RNReanimated;
-});
-
-jest.mock('react-native-worklets', () => ({ runOnJS: (fn: any) => fn }));
-
 // Mock AsyncStorage
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
@@ -27,10 +6,10 @@ jest.mock('@react-native-async-storage/async-storage', () =>
 // Mock DateTimePicker
 jest.mock('@react-native-community/datetimepicker', () => {
   const React = require('react');
-  return React.forwardRef((props: any, ref: any) => (
-    <div data-testid="datetimepicker" {...props}>
-      {props.value && props.value.toString()}
-    </div>
+  const { View, Text } = require('react-native');
+  return React.forwardRef((props, ref) => (
+    React.createElement(View, { testID: 'datetimepicker', ...props }, 
+      props.value && React.createElement(Text, null, props.value.toString())
+    )
   ));
 });
-
